c8dfa0075cc8ca74cd14b0fd473a85c0064b4001
ndksys01 <hung.nguyen@ndksys.com>
2014-06-23 21:32:35 +0700

    Final

--- NhietDo/NhietDo.c
+++ NhietDo/NhietDo.c
@@ -6,7 +6,7 @@
 
    INT temp=0;
 
-char nhietDo[6];
+char nhietDo[4];
 float tmpNhietdo;
 
 void init_adc(INT kenh)
@@ -30,46 +30,37 @@ void init_adc(INT kenh)
 
 void uartInterrupt()
 {
-   temp = read_adc ();
-   tmpNhietdo = (FLOAT) ( ((temp * 5.1 * 100) / 1023));
-   nhietDo[0] = '#';
-   nhietDo[1] = '5';
-   nhietDo[2] = ( ( (INT) tmpNhietdo) % 100) / 10 + 48;
-   nhietDo[3] = ( (INT) tmpNhietdo % 100) % 10 + 48;
-   nhietDo[4] = '.';
-   nhietDo[5] = 0 + 48;
+      output_d (255);
+       delay_ms (500);
+       output_d (0);
+        delay_ms (500);
 }
 
 void main()
 {
    UNSIGNED char a = 0;
    init_adc (1) ;
-   enable_interrupts (global); //Cho phep ngat toan cuc
-   enable_interrupts (INT_rda); //Cho phep ngat UART
+   //enable_interrupts (global); //Cho phep ngat toan cuc
+   //enable_interrupts (INT_rda); //Cho phep ngat UART
    WHILE (1)
    {
       
       temp = read_adc ();
-      tmpNhietdo = (FLOAT) ( ((temp * 5.1 * 100) / 1023));
-      nhietDo[0] = ( ( (INT) tmpNhietdo) % 100) / 10 + 48;
-      nhietDo[1] = ( (INT) tmpNhietdo % 100) % 10 + 48;
-      nhietDo[2] = '.';
-      nhietDo[3] = 48; // (INT) (tmpNhietdo * 10) % 10  +  48;
-      nhietDo[4] = '\r';
-      nhietDo[5] = '\n';
-      //printf (nhietDo);
-      putc ('#'); delay_ms (1) ;
-      putc ('1'); delay_ms (1) ;
-      putc (nhietDo[0]); delay_ms (1) ;
-      putc (nhietDo[1]); delay_ms (1) ;
+      tmpNhietdo = (FLOAT) ( ((temp * 4.0001 * 100) / 1024));
+       nhietDo[0]= '#' ;
+      nhietDo[1]= '1' ;
+      nhietDo[2] = ( ( (INT) tmpNhietdo) % 100) / 10 + 48;
+      nhietDo[3] = ( (INT) tmpNhietdo % 100) % 10 + 48;
+      putc (nhietDo[0]); '#' ;
+      putc (nhietDo[1]); '1' ;
       putc (nhietDo[2]); delay_ms (1) ;
       putc (nhietDo[3]); delay_ms (1) ;
-      putc (nhietDo[4]); delay_ms (1) ;
-      putc (nhietDo[5]); delay_ms (1) ;
-      delay_ms (500); delay_ms (500) ;
       delay_ms (500); delay_ms (500) ;
       output_d (a);
       a = 255 - a;
+
+      
    }
 }
+
 
